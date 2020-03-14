863a0f4df713a5abe37341af91c317a0af1cdbcc
shisulin <929518873@qq.com>
2020-03-15 06:50:09 +0800

    four

--- user.py
+++ user.py
@@ -7,24 +7,24 @@ class User:
 		self.name = name
 		self.email = email
 		self.password_hash = self.save_password(password)
-		
+
 
 	def check_email(self, email):
 		return self.email == email
-		
-		
+
+
 	def save_password(self, password):
 		# TODO
 		return generate_password_hash(password)
-    	
+
 
 	def check_password(self, password):
 
 		# TODO
 		return check_password_hash(self.password_hash,password)
-		
-		
-		
+
+
+
 
 def main():
 
@@ -35,22 +35,22 @@ def main():
 		请选择：
 		1.注册
 		2.登录
-		3.退出	
+		3.退出
 		'''))
 		if choose == 1:
-			print(' 请输入:')           
+			print(' 请输入:')
 			name = input('name:')
 			email = input('email:')
 			password = input('password:')
 
-			#TODO		
+			#TODO
 			newUser = User(name,email,password)
-			userList.append(newUser) 
+			userList.append(newUser)
 			print('注册成功!')
-			
+
 
 		if choose == 2:
-          
+
 			print(' 请输入:')
 			email = input('email:')
 			password = input('password:')
@@ -64,12 +64,13 @@ def main():
 						print('登录成功！')
 					else:
 						print('密码错误！')
+                    break
 			if(TAG == 0):
 				print('用户名错误！')
 
 		if choose == 3:
 			break
-		
+
 
 if __name__ == '__main__':
 	main()
